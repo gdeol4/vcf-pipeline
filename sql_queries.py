e822a8b1b4f0811efbded321f307bbc763b55bbc
Gurkamal Deol <gdeol4@uwo.ca>
2022-06-16 07:50:54 -0400

    Delete sql_queries.py

--- sql_queries.py
+++
@@ -1,6 +0,0 @@
-sql_create_projects_table = """ CREATE TABLE IF NOT EXISTS projects (
-                                        id integer PRIMARY KEY,
-                                        name text NOT NULL,
-                                        begin_date text,
-                                        end_date text
-                                    ); """
