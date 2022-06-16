dabb592830c633dcf1701ec0e858bf69b1a0245e
Gurkamal Deol <gdeol4@uwo.ca>
2022-06-16 14:38:59 -0400

    Add files via upload

--- etl_tools/load.py
+++ etl_tools/load.py
@@ -3,11 +3,12 @@ import pandas as pd
 import urllib
 from io import StringIO
 
+
 def create_db(db_name, annotated_df):
-    
+
     conn = sqlite3.connect(db_name)
 
-    sql_create_variants_table = """ CREATE TABLE IF NOT EXISTS variants (
+    sql_create_variants_table = """ CREATE TABLE IF NOT EXISTS variant (
                                         CHROM text,
                                         POS integer,
                                         ID text,
@@ -20,51 +21,56 @@ def create_db(db_name, annotated_df):
 
     c = conn.cursor()
     c.execute(sql_create_variants_table)
-    annotated_df.to_sql('variants', conn, if_exists='replace', index = False )
+    annotated_df.to_sql('variant', conn, if_exists='replace', index=False)
     conn.close()
 
     return None
 
+
 def get_uniprot_sequences(gene_list) -> pd.DataFrame:
-        """
-        Retrieve uniprot sequences based on a list of uniprot sequence identifier.
+    """
+    Retrieve uniprot sequences based on a list of uniprot sequence identifier.
 
-        For large lists it is recommended to perform batch retrieval.
+    For large lists it is recommended to perform batch retrieval.
 
-        documentation which columns are available:
-        https://www.uniprot.org/help/uniprotkb%5Fcolumn%5Fnames
+    documentation which columns are available:
+    https://www.uniprot.org/help/uniprotkb%5Fcolumn%5Fnames
 
-        this python script is based on
-        https://www.biostars.org/p/67822/
+    this python script is based on
+    https://www.biostars.org/p/67822/
 
-        Parameters:
-            uniprot_ids: List, list of uniprot identifier
+    Parameters:
+        uniprot_ids: List, list of uniprot identifier
 
-        Returns:
-            pd.DataFrame, pandas dataframe with uniprot id column and sequence
-        """
-        
-        url = 'https://www.uniprot.org/uploadlists/'  # This is the webserver to retrieve the Uniprot data
-        params = {
-            'from': "GENENAME",
-            'to': 'ACC',
-            'format': 'tab',
-            'query': " ".join(gene_list),
-            'columns': 'id,sequence'}
+    Returns:
+        pd.DataFrame, pandas dataframe with uniprot id column and sequence
+    """
 
-        data = urllib.parse.urlencode(params)
-        data = data.encode('ascii')
-        request = urllib.request.Request(url, data)
-        with urllib.request.urlopen(request) as response:
-            res = response.read()
-        df_fasta = pd.read_csv(StringIO(res.decode("utf-8")), sep="\t")
-        df_fasta.columns = ["Entry", "Sequence", "Query"]
-        # it might happen that 2 different ids for a single query id are returned, split these rows
-        return df_fasta.assign(Query=df_fasta['Query'].str.split(',')).explode('Query')
+    # This is the webserver to retrieve the Uniprot data
+    url = 'https://www.uniprot.org/uploadlists/'
+    params = {
+        'from': "GENENAME",
+        'to': 'ACC',
+        'format': 'tab',
+        'query': " ".join(gene_list),
+        'columns': 'id,sequence'}
+
+    data = urllib.parse.urlencode(params)
+    data = data.encode('ascii')
+    request = urllib.request.Request(url, data)
+    with urllib.request.urlopen(request) as response:
+        res = response.read()
+    df_fasta = pd.read_csv(StringIO(res.decode("utf-8")), sep="\t")
+    df_fasta.columns = ["Entry", "Sequence", "Query"]
+    # it might happen that 2 different ids for a single query id are returned,
+    # split these rows
+    return df_fasta.assign(
+        Query=df_fasta['Query'].str.split(',')).explode('Query')
+
 
 def process_uniprot(df):
-    df = df.drop_duplicates(subset = 'Query', keep='first')
+    df = df.drop_duplicates(subset='Query', keep='first')
     df = df[["Query", "Entry", "Sequence"]]
-    df.rename(columns = {'Query':'Gene', 'Entry':'Uniprot ID'}, inplace = True)
+    df.rename(columns={'Query': 'Gene', 'Entry': 'Uniprot ID'}, inplace=True)
 
-    return df    
+    return df
--- etl_tools/sql_queries.py
+++ etl_tools/sql_queries.py
@@ -1,64 +1,75 @@
 import sqlite3
 import pandas as pd
 
+
 def query_1(db_name):
 
     conn = sqlite3.connect(db_name)
     c = conn.cursor()
-    results = c.execute('''SELECT CHROM, COUNT(*) 
-                           FROM variant 
-                           GROUP BY CHROM 
-                           ORDER BY COUNT(*) 
+    results = c.execute('''SELECT CHROM, COUNT(*)
+                           FROM variant
+                           GROUP BY CHROM
+                           ORDER BY COUNT(*)
                            DESC LIMIT 1''')
     q1 = pd.DataFrame(results.fetchall())
+    q1.columns = ['Chromosome', 'Variant count']
+
     conn.close()
 
     return q1
 
+
 def query_2(db_name):
     conn = sqlite3.connect(db_name)
     c = conn.cursor()
-    sub_query = '''SELECT CHROM, MIN(start) min_start 
-                   FROM variant 
+    sub_query = '''SELECT CHROM, MIN(start) min_start
+                   FROM variant
                    GROUP BY CHROM'''
-    query = f'''SELECT * 
-                FROM variant a 
+    query = f'''SELECT *
+                FROM variant a
                 INNER JOIN ({sub_query}) b ON a.CHROM = b.CHROM AND a.start = b.min_start'''
     results = c.execute(query)
     q2 = pd.DataFrame(results.fetchall())
+    q2.columns = ['Chromosome', 'ID', 'REF', 'ALT', 'Gene', 'Variant', 'Start', 'Stop', 'Accession', 'GeneID', 'start_y', 'Chromosome_y']
+    q2 = q2.drop(['ID', 'Stop', 'Accession', 'GeneID', 'start_y', 'Chromosome_y'], axis = 1)
     conn.close()
     return q2
+
 
 def query_3(db_name):
 
     conn = sqlite3.connect(db_name)
     c = conn.cursor()
-    results = c.execute('''SELECT CHROM, VC, COUNT(CHROM) FROM variant GROUP BY CHROM, VC;''')
+    results = c.execute(
+        '''SELECT CHROM, VC, COUNT(CHROM) FROM variant GROUP BY CHROM, VC;''')
     q3 = pd.DataFrame(results.fetchall())
-    q3.columns = ['Chromosome', 'VC', 'Count']
+    q3.columns = ['Chromosome', 'Variant', 'Count']
     conn.close()
     return q3
+
 
 def query_4(db_name):
     conn = sqlite3.connect(db_name)
     c = conn.cursor()
-    results = c.execute('''SELECT VC, COUNT(*) 
-                           FROM variant 
-                           GROUP BY VC 
-                           ORDER BY COUNT(*) 
+    results = c.execute('''SELECT VC, COUNT(*)
+                           FROM variant
+                           GROUP BY VC
+                           ORDER BY COUNT(*)
                            DESC LIMIT 1''')
     q4 = pd.DataFrame(results.fetchall())
+    q4.columns = ['Variant type', 'Variant count']
     conn.close()
     return q4
+
 
 def query_5(db_name):
     conn = sqlite3.connect(db_name)
     c = conn.cursor()
-    results = c.execute('''SELECT Gene, COUNT(*) 
-                         FROM variant 
-                         GROUP BY Gene 
+    results = c.execute('''SELECT Gene, COUNT(*)
+                         FROM variant
+                         GROUP BY Gene
                          ORDER BY COUNT(*) DESC''')
     q5 = pd.DataFrame(results.fetchall())
+    q5.columns = ['Gene', 'Variant count']
     conn.close()
     return q5
-
